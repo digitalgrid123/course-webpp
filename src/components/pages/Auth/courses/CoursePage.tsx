2a4c74922c358d600ac20c9c9945ffee3b0535b7
harris <harisrajpoot196@gmail.com>
2025-10-31 18:52:31 +0500

    profile upload added

--- src/components/pages/Auth/courses/CoursePage.tsx
+++ src/components/pages/Auth/courses/CoursePage.tsx
@@ -316,7 +316,7 @@ export default function CoursesPage() {
           </div>
         </div>
 
-        <div className="flex gap-1 lg:col-span-2 justify-end">
+        <div className="flex gap-1 lg:col-span-2 lg:justify-end justify-center">
           <div className="relative" ref={degreeDropdownRef}>
             <button
               className="flex items-center justify-center gap-1 px-4 py-2 rounded-xl   transition-colors min-w-[160px]"
